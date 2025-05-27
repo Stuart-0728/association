de21c1e8cdd004f8c83bbff76aaa4c0d2fb20256
Stuart-0728 <1078184866@qq.com>
2025-05-27 21:44:56 +0800

    Update user.py

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,39 +1,15 @@
-from flask import Blueprint, jsonify, request
-from src.models.user import User, db
+from flask import Blueprint, request, jsonify
+from main import db
+from models.user import User
 
 user_bp = Blueprint('user', __name__)
 
-@user_bp.route('/users', methods=['GET'])
+@user_bp.route('/', methods=['GET'])
 def get_users():
     users = User.query.all()
-    return jsonify([user.to_dict() for user in users])
+    return jsonify([u.to_dict() for u in users])
 
-@user_bp.route('/users', methods=['POST'])
-def create_user():
-    
-    data = request.json
-    user = User(username=data['username'], email=data['email'])
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.to_dict()), 201
-
-@user_bp.route('/users/<int:user_id>', methods=['GET'])
+@user_bp.route('/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify(user.to_dict())
-
-@user_bp.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    data = request.json
-    user.username = data.get('username', user.username)
-    user.email = data.get('email', user.email)
-    db.session.commit()
-    return jsonify(user.to_dict())
-
-@user_bp.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return '', 204
+    u = User.query.get_or_404(user_id)
+    return jsonify(u.to_dict())
